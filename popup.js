00bc8028af5c7decfa1b6a52e84b8a1aa5066c87
SKostyukovich <s.kostyukovich@gmail.com>
2026-03-02 14:52:22 +0300

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,16 @@
+    (function(s, t, e, p, f, o, r, m) {
+        s[t] = s[t] || {};
+        s[t][2101234059] = {
+            id: "XVAdONE",
+            rnd: 2101234059,
+            popup: true,
+            showType: "auto",
+            autoDelay: "onload",
+            autoInterval: "always",
+            overlayColor: "#000000",
+            overlayOpacity: 70
+        };
+        e.async = true;
+        e.src = "https:" + f;
+        document[m](o)[r](e)
+    }(window,"stepFORM_params",document.createElement("script"),"","//u093328.spfrm.com/api.js?id=XVAdONE","head","appendChild","querySelector"));
